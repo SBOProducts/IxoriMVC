b09899aa4f0f29b8308f3c4f01f2ec236f2175db
Nathan Townsend <nathan@nathantownsend.com>
2015-08-21 06:51:16 -0400

    initial commit

---
+++ IxoriMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IxoriMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult AppDevelopment()
+        {
+            return View();
+        }
+
+        public ActionResult WebDevelopment()
+        {
+            return View();
+        }
+
+        public ActionResult Careers()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ IxoriMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IxoriMVC.MvcApplication" Language="C#" %>
